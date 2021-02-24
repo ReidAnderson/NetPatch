86491f6f3c9c019a35c3439e74c954f751c6ba81
ReidAnderson <rbarryanderson@gmail.com>
2021-02-23 22:19:13 -0600

    JSON.NET only used for patch ops

--- NetPatch/PatchHelper.cs
+++ NetPatch/PatchHelper.cs
@@ -14,40 +14,15 @@ namespace NetPatch
     public class PatchHelper
     {
 
-        public static JsonPatchDocument GeneratePatch(string originalJson, string currentJson)
-        {
-            JObject original = JObject.Parse(originalJson);
-            JObject current = JObject.Parse(currentJson);
-
-            JsonPatchDocument patch = new JsonPatchDocument();
-            FillPatchForObject(original, current, patch, "/");
-
-            return patch;
-        }
-
-        public static JsonPatchDocument GeneratePatch(object originalObject, object currentObject)
-        {
-            JObject original = JObject.FromObject(originalObject);
-            JObject current = JObject.FromObject(currentObject);
-
-            JsonPatchDocument patch = new JsonPatchDocument();
-            FillPatchForObject(original, current, patch, "/");
-
-            return patch;
-        }
-
         public static JsonPatchDocument GetPatchForObject(string originalJson, string currentJson)
         {
             JsonPatchDocument patch = new JsonPatchDocument();
 
-            JsonDocument original = JsonDocument.Parse(originalJson);
-            JsonDocument current = JsonDocument.Parse(currentJson);
-            
-            // using (JsonDocument original = JsonDocument.Parse(originalJson))
-            // using (JsonDocument current = JsonDocument.Parse(currentJson))
-            // {
-                FindPatchDiffs(original.RootElement, current.RootElement, patch, "/");
-            // }
+            using (JsonDocument original = JsonDocument.Parse(originalJson))
+            using (JsonDocument current = JsonDocument.Parse(currentJson))
+            {
+                FindPatchDiffs(original.RootElement.Clone(), current.RootElement.Clone(), patch, "/");
+            }
 
             return patch;
         }
@@ -56,105 +31,81 @@ namespace NetPatch
         // for this property: path + prop.Name
         private static void FindPatchDiffs(JsonElement original, JsonElement current, JsonPatchDocument patchDocument, string path)
         {
+            if (original.ValueKind != JsonValueKind.Object && original.ValueKind != JsonValueKind.Array)
+            {
+                throw new Exception("Value types should not require a recursive call");
+            }
+
             HashSet<string> currentPropSet = new HashSet<string>(current.EnumerateObject().Select(eo => eo.Name));
             HashSet<string> originalPropSet = new HashSet<string>(original.EnumerateObject().Select(eo => eo.Name));
-
 
             // Start by checking for properties that have just been added or removed
             if (original.ValueKind == JsonValueKind.Object)
             {
-                foreach(JsonProperty property in original.EnumerateObject()) {
-                    if (!currentPropSet.Contains(property.Name)) {
+                foreach (JsonProperty property in original.EnumerateObject())
+                {
+                    if (!currentPropSet.Contains(property.Name))
+                    {
                         patchDocument.Remove($"{path}{property.Name}");
                     }
                 }
 
-                foreach(JsonProperty property in current.EnumerateObject()) {
-                    if (!originalPropSet.Contains(property.Name)) {
-                        AddAsType(property.Value, patchDocument, $"{path}{property.Name}", current, property);
-                    }
-                }
-            }
-
-        }
-
-        private static void AddAsType(JsonElement element, JsonPatchDocument patchDocument, string path, JsonElement parentObject, JsonProperty property) {
-            switch (element.ValueKind) {
-                case JsonValueKind.Null:
-                    patchDocument.Add(path, null);
-                    break;
-                case JsonValueKind.Object:
-                    patchDocument.Add(path, JObject.Parse(parentObject.GetRawText()).Property(property.Name).Value);
-                    break;
-                case JsonValueKind.Array:
-                    patchDocument.Add(path, JObject.Parse(parentObject.GetRawText()).Property(property.Name).Value);
-                    break;
-                case JsonValueKind.Number:
-                    patchDocument.Add(path, element.GetDouble());
-                    break;
-                case JsonValueKind.True:
-                    patchDocument.Add(path, true);
-                    break;
-                case JsonValueKind.False:
-                    patchDocument.Add(path, false);
-                    break;
-                case JsonValueKind.String:
-                    patchDocument.Add(path, element.GetString());
-                    break;
-                case JsonValueKind.Undefined:
-                    patchDocument.Add(path, null);
-                    break;
-                default:
-                    throw new Exception("Unsupported kind");
-            }
-        }
-
-        static void FillPatchForObject(JObject original, JObject current, JsonPatchDocument patch, string path)
-        {
-            string[] originalNames = original.Properties().Select(x => x.Name).ToArray();
-            string[] currentNames = current.Properties().Select(x => x.Name).ToArray();
-
-            // Properties that have been removed
-            foreach (string k in originalNames.Except(currentNames))
-            {
-                JProperty prop = original.Property(k);
-                patch.Remove(path + prop.Name);
-            }
-
-            // Properties that have been added
-            foreach (string k in currentNames.Except(originalNames))
-            {
-                JProperty prop = current.Property(k);
-                patch.Add(path + prop.Name, prop.Value);
-            }
-
-            // Present in both
-            foreach (string k in originalNames.Intersect(currentNames))
-            {
-                JProperty originalProperty = original.Property(k);
-                JProperty currentProperty = current.Property(k);
-
-                if (originalProperty.Value.Type != currentProperty.Value.Type)
+                foreach (JsonProperty property in current.EnumerateObject())
                 {
-                    // If the types don't match, this must be a replace
-                    patch.Replace(path + currentProperty.Name, currentProperty.Value);
-                }
-                else if (!string.Equals(
-                                originalProperty.Value.ToString(Newtonsoft.Json.Formatting.None),
-                                currentProperty.Value.ToString(Newtonsoft.Json.Formatting.None)))
-                {
-                    // If the JSON string representation is different, some kind of action is needed
-                    if (originalProperty.Value.Type == JTokenType.Object)
+                    if (!originalPropSet.Contains(property.Name))
                     {
-                        // Recurse into objects
-                        FillPatchForObject(originalProperty.Value as JObject, currentProperty.Value as JObject, patch, path + currentProperty.Name + "/");
+                        patchDocument.Add($"{path}{property.Name}", AddAsType(property.Value, patchDocument, current, property));
                     }
                     else
                     {
-                        // Replace values directly
-                        patch.Replace(path + currentProperty.Name, currentProperty.Value);
+                        JsonElement originalPropertyValue = original.GetProperty(property.Name);
+
+                        if (property.Value.ValueKind != originalPropertyValue.ValueKind)
+                        {
+                            // Different types => replace
+                            patchDocument.Replace($"{path}{property.Name}", AddAsType(property.Value, patchDocument, current, property));
+                        }
+
+                        if (property.Value.GetRawText() != originalPropertyValue.GetRawText())
+                        {
+                            if (property.Value.ValueKind == JsonValueKind.Object)
+                            {
+                                FindPatchDiffs(originalPropertyValue, property.Value, patchDocument, $"{path}{property.Name}/");
+                            }
+                            else
+                            {
+                                patchDocument.Replace($"{path}{property.Name}", AddAsType(property.Value, patchDocument, current, property));
+                            }
+
+                        }
                     }
                 }
+            }
+
+        }
+
+        private static object AddAsType(JsonElement element, JsonPatchDocument patchDocument, JsonElement parentObject, JsonProperty property)
+        {
+            switch (element.ValueKind)
+            {
+                case JsonValueKind.Null:
+                    return null;
+                case JsonValueKind.Object:
+                    return JObject.Parse(parentObject.GetRawText()).Property(property.Name).Value;
+                case JsonValueKind.Array:
+                    return JObject.Parse(parentObject.GetRawText()).Property(property.Name).Value;
+                case JsonValueKind.Number:
+                    return element.GetDouble();
+                case JsonValueKind.True:
+                    return true;
+                case JsonValueKind.False:
+                    return false;
+                case JsonValueKind.String:
+                    return element.GetString();
+                case JsonValueKind.Undefined:
+                    return null;
+                default:
+                    throw new Exception("Unsupported kind");
             }
         }
     }
--- NetPatchTests/PatchHistoryTests.cs
+++ NetPatchTests/PatchHistoryTests.cs
@@ -9,16 +9,6 @@ namespace NetPatchTests
     public class NetPatchTests
     {
         [Fact]
-        public void Sample()
-        {
-            var patch = PatchHelper.GeneratePatch(
-                        new { Unchanged = new[] { 1, 2, 3, 4, 5 }, Changed = "1", Removed = "1" },
-                        new { Unchanged = new[] { 1, 2, 3, 4, 5 }, Changed = "2", Added = new { x = "1" } });
-
-            Console.WriteLine(JsonConvert.SerializeObject(patch));
-        }
-
-        [Fact]
         public void BasicSystemTextJsonExample() {
             var patch = PatchHelper.GetPatchForObject("{}", "{}");
             Assert.Equal(1,1);
