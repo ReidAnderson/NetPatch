04b8bd7fb1064a1d1546285fe9b24942c8bf7faa
ReidAnderson <rbarryanderson@gmail.com>
2021-02-28 13:22:52 -0600

    Working with more complex objects

--- NetPatch/PatchHelper.cs
+++ NetPatch/PatchHelper.cs
@@ -7,10 +7,16 @@ using System.Text.Json;
 using netjson = System.Text.Json;
 using System.Diagnostics;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 
 namespace NetPatch
 {
+    public enum Operation {
+        Add,
+        Remove
+    }
+
     public class PatchHelper
     {
 
@@ -54,7 +60,7 @@ namespace NetPatch
                 {
                     if (!originalPropSet.Contains(property.Name))
                     {
-                        patchDocument.Add($"{path}{property.Name}", AddAsType(property.Value, patchDocument, current, property));
+                        patchDocument.Add($"{path}{property.Name}", AddAsType(property.Value, patchDocument));
                     }
                     else
                     {
@@ -63,7 +69,7 @@ namespace NetPatch
                         if (property.Value.ValueKind != originalPropertyValue.ValueKind)
                         {
                             // Different types => replace
-                            patchDocument.Replace($"{path}{property.Name}", AddAsType(property.Value, patchDocument, current, property));
+                            patchDocument.Replace($"{path}{property.Name}", AddAsType(property.Value, patchDocument));
                         }
 
                         if (property.Value.GetRawText() != originalPropertyValue.GetRawText())
@@ -74,7 +80,7 @@ namespace NetPatch
                             }
                             else
                             {
-                                patchDocument.Replace($"{path}{property.Name}", AddAsType(property.Value, patchDocument, current, property));
+                                patchDocument.Replace($"{path}{property.Name}", AddAsType(property.Value, patchDocument));
                             }
                         }
                     }
@@ -86,6 +92,15 @@ namespace NetPatch
                 List<JsonElement> originalElements = original.EnumerateArray().Select(o => o).ToList();
                 List<JsonElement> currentElements = current.EnumerateArray().Select(c => c).ToList();
 
+                Tuple<int?, Operation?> isAddOrRemove = IsAddOrRemove(originalElements, currentElements);
+                if (isAddOrRemove.Item2 == Operation.Add && isAddOrRemove.Item1.HasValue) {
+                    patchDocument.Add($"{path}{isAddOrRemove.Item1}", AddAsType(currentElements[isAddOrRemove.Item1.Value], patchDocument));
+                    return;
+                } else if (isAddOrRemove.Item2 == Operation.Remove && isAddOrRemove.Item1.HasValue) {
+                    patchDocument.Remove($"{path}{isAddOrRemove.Item1}");
+                    return;
+                }
+
                 if (currentElements.Count() > originalElements.Count())
                 {
                     for (int i = originalElements.Count(); i < currentElements.Count(); i++)
@@ -94,38 +109,38 @@ namespace NetPatch
                         if (i == currentElements.Count() - 1){
                             pathIdentifier = "-";
                         }
-                        patchDocument.Add($"{path}{pathIdentifier}", AddAsType(currentElements[i], patchDocument, null, null));
+                        patchDocument.Add($"{path}{pathIdentifier}", AddAsType(currentElements[i], patchDocument));
                     }
                 }
 
                 for (int i = 0; i < originalElements.Count(); i++)
                 {
-                    if (i > currentElements.Count())
+                    if (i >= currentElements.Count())
                     {
                         patchDocument.Remove($"{path}{i}");
+                        continue;
                     }
 
                     if (originalElements[i].GetRawText() != currentElements[i].GetRawText())
                     {
-                        patchDocument.Replace($"{path}{i}", AddAsType(currentElements[i], patchDocument, null, null));
+                        patchDocument.Replace($"{path}{i}", AddAsType(currentElements[i], patchDocument));
                     }
                 }
             }
-
         }
 
-        private static object AddAsType(JsonElement element, JsonPatchDocument patchDocument, JsonElement? parentObject, JsonProperty? property)
+        private static object AddAsType(JsonElement element, JsonPatchDocument patchDocument)
         {
             switch (element.ValueKind)
             {
                 case JsonValueKind.Null:
                     return null;
                 case JsonValueKind.Object:
-                    return parentObject == null ? JObject.Parse(element.GetRawText()) : JObject.Parse(parentObject.Value.GetRawText()).Property(property.Value.Name).Value;
+                    return JsonConvert.DeserializeObject<ExpandoObject>(element.GetRawText());
                 case JsonValueKind.Array:
-                    return parentObject == null ? JArray.Parse(element.GetRawText()) : JObject.Parse(parentObject.Value.GetRawText()).Property(property.Value.Name).Value;
+                    return JArray.Parse(element.GetRawText());
                 case JsonValueKind.Number:
-                    return element.GetDouble();
+                    return JToken.Parse(element.GetRawText());
                 case JsonValueKind.True:
                     return true;
                 case JsonValueKind.False:
@@ -136,6 +151,50 @@ namespace NetPatch
                     return null;
                 default:
                     throw new Exception("Unsupported kind");
+            }
+        }
+
+        private static List<int> GetShiftArray(List<JsonElement> originalElements, List<JsonElement> currentElements) {
+            List<int> shiftFromOriginal = new List<int>();
+            for (int i = 0; i < originalElements.Count(); i++) {
+                for (int j = 0; j < currentElements.Count(); j++) {
+                    if (originalElements[i].GetRawText() == currentElements[j].GetRawText()) {
+                        shiftFromOriginal.Add(j-i);
+                    }
+                }
+            }
+
+            return shiftFromOriginal;
+        }
+
+        private static Tuple<int?, Operation?> IsAddOrRemove(List<JsonElement> originalElements, List<JsonElement> currentElements) {
+            bool hasChanged = false;
+            int? changepoint = null;
+            Operation? op = null;
+
+            List<int> shiftArray = GetShiftArray(originalElements, currentElements);
+
+            for (int i = 1; i < shiftArray.Count(); i++) {
+                if (shiftArray[i] > 0) {
+                    op = Operation.Add;
+                } else if (shiftArray[i] < 0) {
+                    op = Operation.Remove;
+                }
+
+                if (shiftArray[i -1] != shiftArray[i]) {
+                    if (hasChanged) {
+                        return new Tuple<int?, Operation?>(null, null);
+                    } else {
+                        changepoint = i;
+                        hasChanged = true;
+                    }
+                }
+            }
+
+            if (hasChanged) {
+                return new Tuple<int?, Operation?>(changepoint, op);
+            } else {
+                return new Tuple<int?, Operation?>(null, null);
             }
         }
     }
