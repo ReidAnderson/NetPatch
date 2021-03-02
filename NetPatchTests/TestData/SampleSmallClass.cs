6980bcfdc6f7a6a388b70e97c45cc891b3d04d1c
ReidAnderson <rbarryanderson@gmail.com>
2021-03-01 20:25:37 -0600

    Incredibly simple performance test

---
+++ NetPatchTests/TestData/SampleSmallClass.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace NetPatchTests.TestData
+{
+    public class SampleSmallClass
+    {
+        public string Property1 { get; set; }
+        public string Property2 { get; set; }
+        public int Number1 { get; set; }
+        public bool Boolean1 { get; set; }
+        public List<string> List1 { get; set; }
+        public Dictionary<string, int> Dictionary1 { get; set; }
+    }
+}
